95657dc2c8b8685dd6d5cd38e2c73963f658a44c
kathyguan <kathy1deagosto@gmail.com>
2025-01-02 19:19:47 -0500

    Usuarios

---
+++ bibliotecaMusica/Usuarios.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="Formulario de Usuario" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Usuarios.aspx.cs" Inherits="bibliotecaMusica.Usuarios" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+        <h1 class="text-center">Formulario de Usuario</h1>
+
+        <div class="form-container">
+            <div class="form-group">
+                <asp:Label ID="lbl_nombre" runat="server" Text="Nombre" CssClass="form-label"></asp:Label>
+                <asp:TextBox ID="txt_nombre" runat="server" CssClass="form-input" Placeholder="Ingrese su nombre" MaxLength="100" Required="true"></asp:TextBox>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lbl_apellido" runat="server" Text="Apellido" CssClass="form-label"></asp:Label>
+                <asp:TextBox ID="txt_apellido" runat="server" CssClass="form-input" Placeholder="Ingrese su apellido" MaxLength="100" Required="true"></asp:TextBox>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lbl_email" runat="server" Text="Email" CssClass="form-label"></asp:Label>
+                <asp:TextBox ID="txt_email" runat="server" CssClass="form-input" Placeholder="Ingrese su email" MaxLength="150" Required="true"></asp:TextBox>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lbl_contrasena" runat="server" Text="Contraseña" CssClass="form-label"></asp:Label>
+                <asp:TextBox ID="txt_contrasena" runat="server" CssClass="form-input" Placeholder="Ingrese su contraseña" TextMode="Password" MaxLength="255" Required="true"></asp:TextBox>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lbl_rol" runat="server" Text="Rol" CssClass="form-label"></asp:Label>
+                <asp:DropDownList ID="ddl_rol" runat="server" CssClass="form-input">
+                    <asp:ListItem Text="Seleccionar Rol" Value=""></asp:ListItem>
+                    <asp:ListItem Text="Administrador" Value="Administrador"></asp:ListItem>
+                    <asp:ListItem Text="Usuario" Value="Usuario"></asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lbl_fecha_registro" runat="server" Text="Fecha de Registro" CssClass="form-label"></asp:Label>
+                <asp:TextBox ID="txt_fecha_registro" runat="server" CssClass="form-input" Text='<%# DateTime.Now.ToString("dd/MM/yyyy") %>' Enabled="false"></asp:TextBox>
+            </div>
+
+            <div class="form-group">
+                <asp:Button ID="btn_guardar" runat="server" Text="Guardar" CssClass="btn btn-primary" OnClick="btn_guardar_Click" />
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ bibliotecaMusica/Usuarios.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace bibliotecaMusica
+{
+
+
+    public partial class Usuarios
+    {
+
+        /// <summary>
+        /// Control lbl_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_nombre;
+
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+
+        /// <summary>
+        /// Control lbl_apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_apellido;
+
+        /// <summary>
+        /// Control txt_apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_apellido;
+
+        /// <summary>
+        /// Control lbl_email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_email;
+
+        /// <summary>
+        /// Control txt_email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_email;
+
+        /// <summary>
+        /// Control lbl_contrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_contrasena;
+
+        /// <summary>
+        /// Control txt_contrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_contrasena;
+
+        /// <summary>
+        /// Control lbl_rol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_rol;
+
+        /// <summary>
+        /// Control ddl_rol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_rol;
+
+        /// <summary>
+        /// Control lbl_fecha_registro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_fecha_registro;
+
+        /// <summary>
+        /// Control txt_fecha_registro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_fecha_registro;
+
+        /// <summary>
+        /// Control btn_guardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_guardar;
+    }
+}
